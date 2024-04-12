6170a65339f851877d7047dfc7ea5dfb9a88c1f7
Ifaz Md Zahin <zahin26112004@gmail.com>
2024-04-12 11:41:06 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+var typing = new Typed(".text", {
+    strings: ["", "creators.", "editors."],
+    typeSpeed: 100,
+    backSpeed: 40,
+    loop: true
+  });
+  
+  gsap.registerPlugin(ScrollTrigger);
+  const introsplitTypes = document.querySelectorAll(".left-part h1");
+  introsplitTypes.forEach((char, i) => {
+    const i_text = new SplitType(char);
+    gsap.to(i_text.chars, {
+      y: 0,
+      stagger: 0.08,
+      duration: 0.3
+    });
+  });
+  
